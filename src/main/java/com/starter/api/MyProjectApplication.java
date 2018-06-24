f0a98d2256756929129f4aa6ee1dfed78b79fc29
carlos.oliveira <carlosalberto6791@gmail.com>
2018-06-24 20:23:08 -0300

    alteracao no .

--- src/main/java/com/starter/api/MyProjectApplication.java
+++ src/main/java/com/starter/api/MyProjectApplication.java
@@ -8,6 +8,7 @@ public class MyProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>..FOIIIIIIIIII");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>..funfouuuuuuuuuuuu");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>..funfouuuuuuuuuuuu mesmo...");
 	}
 }
